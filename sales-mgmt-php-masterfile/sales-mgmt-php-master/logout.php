89c4e0caf562babe93c54bcec8a8acede5a73afd
c4 <oregecriffin@outlook.com>
2023-03-01 16:21:52 +0300

    sss

--- sales-mgmt-php-masterfile/sales-mgmt-php-master/logout.php
+++
@@ -1,6 +0,0 @@
-<?php 
-	session_start();
-	$_SESSION['username'];
-	session_destroy();
-	echo "<script>location.replace('index.php')</script>";
-?>
--- sales-mgmt-php-masterfile/sales-mgmt-php-master/username.php
+++
@@ -1,7 +0,0 @@
-<div class="username">
-
-		<span class="left">Hello! </span>
-		
-		<span class="right"><a href="logout.php">Logout </a></span>
-
-</div>
